681c566f42db46616e65be54568a0d9e0e250d5d
Taisuke Fukuno <fukuno@jig.jp>
2024-05-10 09:21:42 +0900

    add fetch test

--- test/M3U8.test.js
+++ test/M3U8.test.js
@@ -1,16 +1,21 @@
 import * as t from "https://deno.land/std/testing/asserts.ts";
 import { M3U8 } from "../M3U8.js";
 
-Deno.test("simple", async () => {
+Deno.test("from file", async () => {
   const m3u8 = await M3U8.fetch("./test.m3u8");
   const files = m3u8.getFiles();
   t.assertEquals(files, ["test1.jpg", "test2.jpg"]);
 });
-Deno.test("json", async () => {
+Deno.test("from json", async () => {
   const m3u8 = new M3U8([
     { file: "test1.jpg" },
     { file: "test2.jpg" },
   ]);
+  const files = m3u8.getFiles();
+  t.assertEquals(files, ["test1.jpg", "test2.jpg"]);
+});
+Deno.test("via fetch", async () => {
+  const m3u8 = await M3U8.fetch("https://code4fukui.github.io/M3U8/test/test.m3u8");
   const files = m3u8.getFiles();
   t.assertEquals(files, ["test1.jpg", "test2.jpg"]);
 });
